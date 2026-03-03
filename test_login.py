d33a3964aa7a1c167ad79f7fc8fc4331d0b2b765
CRISTHIAN LIZCANO <cristhianrafael.lizcano@gmail.com>
2026-03-03 17:09:24 -0500

    clean up test files

--- test_login.py
+++
@@ -1,7 +0,0 @@
-from fastapi.testclient import TestClient
-from main import app
-
-client = TestClient(app)
-response = client.post("/auth/login", data={"username": "admin@saas.com", "password": "password123"})
-print(response.status_code)
-print(response.json())
